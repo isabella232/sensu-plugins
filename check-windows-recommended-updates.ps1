7fdb726872f76cba7afb2bb37b530f131dbd83e0
Brian Abreu <brian@nutsonline.com>
2014-11-12 16:06:43 -0800

    Add check-windows-recommended-updates.ps1

---
+++ check-windows-recommended-updates.ps1
@@ -0,0 +1,12 @@
+$updateSession = new-object -com "Microsoft.Update.Session"
+$allUpdates = $updateSession.CreateupdateSearcher().Search("IsInstalled=0 and Type='Software'").Updates
+$recommendedUpdates = $allUpdates | ? { $_.AutoSelectOnWebSites }
+
+if ($recommendedUpdates.Count -eq 0) {
+	Write-Host "OK - no pending updates."
+	exit 0
+}
+else {
+	Write-Host "ALERT - "  (($recommendedUpdates).Title -Join "`n")
+	exit 3
+}
